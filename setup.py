778efb0b8f797dc3c1289ef661b6d42c63da9ae0
Dan <dan.r.lucas@gmail.com>
2023-10-29 19:35:02 -0400

    2023-10-29 19:34pm commit 005: changed API key/org again

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from skllm.config import SKLLMConfig
 
 key = input('enter openAI key:\n')
+org = input('enter openAI organization ID: \n')
 
 SKLLMConfig.set_openai_key(f'{key}')
-SKLLMConfig.set_openai_org("org-lFhItV8DbQoI0lkfgqpxnrLm")
-print('ran setup.py')
+SKLLMConfig.set_openai_org(f'{org}')
+print('finished setup')
